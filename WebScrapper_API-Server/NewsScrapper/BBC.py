3d5a9cf671123191f1341f6aaf92d0c9e9e40e06
ArMonarch <praffulthapa12@gmail.com>
2024-09-25 09:36:35 +0545

    Starting News Scrapping

---
+++ WebScrapper_API-Server/NewsScrapper/BBC.py
@@ -0,0 +1,8 @@
+class BBC:
+
+    BBC_Technology_News_URL = ""
+    BBC_ScienceHealth_News_URL = ""
+    BBC_AI_NEWS_URL = ""
+
+    def __init__(self):
+        print("This is BBC news scrapper!!")
---
+++ WebScrapper_API-Server/main.py
@@ -0,0 +1,3 @@
+from NewsScrapper.BBC import BBC
+
+BBC1 = BBC()
