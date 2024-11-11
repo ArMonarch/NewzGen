0be6f16e00f8f7f52efe6283bbbba47a571f6b9a
ArMonarch <praffulthapa12@gmail.com>
2024-11-11 11:30:52 +0545

    chore: Completed BCC news route to get news

--- WebScrapper_API-Server/views.py
+++ WebScrapper_API-Server/views.py
@@ -1,22 +1,43 @@
 from flask import Blueprint, request
 from NewsScrapper.BBC import BBC
+from typing import Dict, List
 
 # create blueprint for the views
 api = Blueprint(name='WebScrapper',import_name='api')
 
-@api.route('/get/article', methods=['GET'])
+@api.route('/bbc/get/article', methods=['GET'])
 def getArticle():
-    if request.method != 'GET':
-        # raise Exception("METHOD ERROR: route only supports GET method")
-        return 'METHOD ERROR: route only supports GET method'
-    
-    if request.args.get('topic') == None:
-        # raise Exception('QUERY ERROR: For this api Query args should be topic')
-        return 'QUERY ERROR: For this api Query args should be topic'
-    
-    Article = BBC().getArticle()
-    return Article
+    try:
+        if request.method != 'GET':
+            # raise Exception("METHOD ERROR: route only supports GET method")
+            raise Exception('METHOD ERROR: route only supports GET method')
 
-@api.route('get/articles',methods=['GET'])
+        if request.args.get('topic') == None:
+            # raise Exception('QUERY ERROR: For this api Query args should be topic')
+            raise Exception('QUERY ERROR: For this api Query args should be topic')
+    
+        Article: Dict = BBC().getArticle()
+        return Article
+    except Exception as e:
+        return e
+
+@api.route('/bbc/get/articles',methods=['GET'])
 def getArticles():
-    return '200'
+    try:
+        if request.method != 'GET':
+            # raise Exception("METHOD ERROR: route only supports GET method")
+            raise Exception('METHOD ERROR: route only supports GET method')
+
+        if request.args.get('topic') == None:
+            # raise Exception('QUERY ERROR: For this api Query args should be topic')
+            raise Exception('QUERY ERROR: For this api Query args should be topic')
+        
+        if int(request.args.get('size')) > 1:
+            Articles: List[Dict] = BBC().getArticles(request.args.get('size'))
+            return Articles
+        elif request.args.get('size') == None:
+            Articles: List[Dict] = BBC().getArticles(2)
+            return Articles      
+
+    except Exception as e:
+        return e
